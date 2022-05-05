9bb696b9596c943f396ba750845da31fa5eee70c
rpopov <rpopov@jinr.ru>
2022-05-05 19:09:40 +0300

    add postgre container

--- config.py
+++ config.py
@@ -1,9 +1,19 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #    'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = \
+    'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}'.format(
+        user=os.environ.get('DBUSER'),
+        passwd=os.environ.get('DBPASS'),
+        host=os.environ.get('DBHOST'),
+        port=os.environ.get('DBPORT'),
+        db=os.environ.get('DBNAME'))
