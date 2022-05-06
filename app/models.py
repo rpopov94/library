c6ddd5b7f3c90853bc2c427a58215c753c2300a2
rpopov <rpopov@jinr.ru>
2022-05-06 14:28:38 +0300

    models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,4 @@
-from sqlalchemy import ForeignKey
 from app import db
-from sqlalchemy.dialects.sqlite import BLOB
 
 
 class User(db.Model):
@@ -13,18 +11,18 @@ class User(db.Model):
         return '<User {}>'.format(self.username) 
 
 class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(50))
     title = db.Column(db.String(64), index=True, unique=True)
     publisher = db.Column(db.String(64), index=True, unique=True)
     year = db.Column(db.Integer)
     level = db.relationship('Level', backref='book', lazy='dynamic')
-    tag  = db.relationship("Tag", backref='book', lazy="dynamic")
-    book_file  = db.Column(BLOB)
+    tag = db.relationship("Tag", backref='book', lazy="dynamic")
+    book_file  = db.Column(db.LargeBinary)
 
-class Level(db.Model):
+class Levels(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    level_id = db.Column(db.String(64), ForeignKey('book.id'))
+    level_id = db.Column(db.String(50), db.ForeignKey('book.id'))
 
-class Tag(db.Model):
+class Tags(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tag_id = db.Column(db.String(64), ForeignKey('book.id'))
+    tag_id = db.Column(db.String(50), db.ForeignKey('book.id'))
